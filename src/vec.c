c3879fd4ff236b37cc58b18f7868d51be3a60f75
Gianni Tedesco <gianni@scaramanga.co.uk>
2009-12-03 15:36:30 +0000

    Holy shit forgot to track vec.c

---
+++ src/vec.c
@@ -0,0 +1,105 @@
+/*
+* This file is part of Firestorm NIDS
+* Copyright (c) 2003,2004 Gianni Tedesco
+* This program is released under the terms of the GNU GPL version 2
+*/
+
+#include <firestorm.h>
+#include <ctype.h>
+
+/* Convert a vector string in to an unsigned int */
+size_t vtouint(struct ro_vec *v, unsigned int *u)
+{
+	const uint8_t *chr = v->v_ptr;
+	size_t i;
+
+	*u = 0;
+
+	for(i=0; i < v->v_len; i++) {
+		unsigned int digit = *chr++ - '0';
+		if ( digit >= 10 )
+			break;
+
+		*u *= 10;
+		*u += digit;
+	}
+
+	return i;
+}
+
+/* Case-insensitive comparison of two vectors */
+int vcasecmp(const struct ro_vec *v1, const struct ro_vec *v2)
+{
+	const uint8_t *end1, *end2;
+	const uint8_t *p1, *p2;
+
+	end1 = v1->v_ptr + v1->v_len;
+	end2 = v2->v_ptr + v2->v_len;
+
+	p1 = v1->v_ptr;
+	p2 = v2->v_ptr;
+
+	for(;;) {
+		if ( p1 == end1 && p2 == end2 )
+			return 0;
+
+		if ( p1 == end1 )
+			return -1;
+
+		if ( p2 == end2 )
+			return 1;
+
+		if ( toupper(*p1) < toupper(*p2) )
+			return -1;
+
+		if ( toupper(*p1) > toupper(*p2) )
+			return 1;
+
+		p1++; p2++;
+	}
+	
+}
+
+/* Case-sensitive comparison of two vectors */
+int vcmp(const struct ro_vec *v1, const struct ro_vec *v2)
+{
+	const uint8_t *end1, *end2;
+	const uint8_t *p1, *p2;
+
+	end1 = v1->v_ptr + v1->v_len;
+	end2 = v2->v_ptr + v2->v_len;
+
+	p1 = v1->v_ptr;
+	p2 = v2->v_ptr;
+
+	for(;;) {
+		if ( p1 == end1 && p2 == end2 )
+			return 0;
+
+		if ( p1 == end1 )
+			return -1;
+
+		if ( p2 == end2 )
+			return 1;
+
+		if ( *p1 < *p2 )
+			return -1;
+
+		if ( *p1 > *p2 )
+			return 1;
+
+		p1++; p2++;
+	}
+	
+}
+
+/* Case-insensitive comparison of a string and a vector */
+int vstrcmp(const struct ro_vec *v1, const char *str)
+{
+	struct ro_vec v2;
+
+	v2.v_ptr = (void *)str;
+	v2.v_len = strlen(str);
+
+	return vcasecmp(v1, &v2);
+}
