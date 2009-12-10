06da18e341cfade1c648efc74a2e5eec11a577d6
Gianni Tedesco <gianni@scaramanga.co.uk>
2009-12-10 13:55:47 +0000

    Simplify and optimizie vector functions

--- src/vec.c
+++ src/vec.c
@@ -10,74 +10,55 @@
 /* Convert a vector string in to an unsigned int */
 size_t vtouint(struct ro_vec *v, unsigned int *u)
 {
-	const uint8_t *chr = v->v_ptr;
-	size_t i;
+	const uint8_t *ptr, *end;
 
-	*u = 0;
+	for((*u) = 0, ptr = v->v_ptr, end = ptr + v->v_len; ptr < end; ptr++) {
+		unsigned int digit = (*ptr) - '0';
 
-	for(i=0; i < v->v_len; i++) {
-		unsigned int digit = *chr++ - '0';
 		if ( digit >= 10 )
 			break;
 
-		*u *= 10;
-		*u += digit;
+		(*u) *= 10;
+		(*u) += digit;
 	}
 
-	return i;
+	return ptr - v->v_ptr;
 }
 
 /* Case-insensitive comparison of two vectors */
 int vcasecmp(const struct ro_vec *v1, const struct ro_vec *v2)
 {
 	size_t idx;
+	ssize_t ret;
 
-	if ( v1->v_len < v2->v_len )
-		return -1;
-	if ( v1->v_len > v2->v_len )
-		return 1;
-
-	for(idx = 0; idx < v1->v_len; idx++) {
-		int ret;
-		ret = tolower(v1->v_ptr[idx]) - tolower(v2->v_ptr[idx]);
-		if ( ret )
-			return ret;
+	ret = v1->v_len - v2->v_len;
+	if ( 0 == ret ) {
+		for(idx = 0; idx < v1->v_len; idx++) {
+			ret = tolower(v1->v_ptr[idx]) - tolower(v2->v_ptr[idx]);
+			if ( ret )
+				break;
+		}
 	}
 
-	return 0;
+	return ret;
 }
 
 /* Case-sensitive comparison of two vectors */
 int vcmp(const struct ro_vec *v1, const struct ro_vec *v2)
 {
-	const uint8_t *end1, *end2;
-	const uint8_t *p1, *p2;
+	size_t idx;
+	ssize_t ret;
 
-	end1 = v1->v_ptr + v1->v_len;
-	end2 = v2->v_ptr + v2->v_len;
-
-	p1 = v1->v_ptr;
-	p2 = v2->v_ptr;
-
-	for(;;) {
-		if ( p1 == end1 && p2 == end2 )
-			return 0;
-
-		if ( p1 == end1 )
-			return -1;
-
-		if ( p2 == end2 )
-			return 1;
-
-		if ( *p1 < *p2 )
-			return -1;
-
-		if ( *p1 > *p2 )
-			return 1;
-
-		p1++; p2++;
+	ret = v1->v_len - v2->v_len;
+	if ( 0 == ret ) {
+		for(idx = 0; idx < v1->v_len; idx++) {
+			ret = v1->v_ptr[idx] - v2->v_ptr[idx];
+			if ( ret )
+				break;
+		}
 	}
-	
+
+	return ret;
 }
 
 /* Case-insensitive comparison of a string and a vector */
